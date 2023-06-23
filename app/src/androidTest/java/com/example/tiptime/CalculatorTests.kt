c591b3becfec3e4ca5773574aecd8a246da5996b
Cody Viscardis <codyviscardis@gmail.com>
2023-06-23 12:21:33 -0500

    Added additional CalculatorTests functions for all radio buttons and round up switch.

--- app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -5,8 +5,7 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.CoreMatchers.containsString
@@ -31,5 +30,83 @@ class CalculatorTests {
 
         onView(withId(R.id.tip_result))
             .check(matches(withText(containsString("$10.00"))))
+    }
+
+    @Test
+    fun calculate_18_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.option_eighteen_percent))
+            .perform(click())
+
+        onView(withId(R.id.option_eighteen_percent))
+            .check(matches(isChecked()))
+
+        onView(withId(R.id.option_twenty_percent))
+            .check(matches(isNotChecked()))
+
+        onView(withId(R.id.option_fifteen_percent))
+            .check(matches(isNotChecked()))
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId((R.id.tip_result)))
+            .check(matches(withText(containsString("$9.00"))))
+    }
+
+    @Test
+    fun calculate_15_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.option_fifteen_percent))
+            .perform(click())
+
+        onView(withId(R.id.option_fifteen_percent))
+            .check(matches(isChecked()))
+
+        onView(withId(R.id.option_twenty_percent))
+            .check(matches(isNotChecked()))
+
+        onView(withId(R.id.option_eighteen_percent))
+            .check(matches(isNotChecked()))
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId((R.id.tip_result)))
+            .check(matches(withText(containsString("$7.50"))))
+    }
+
+    @Test
+    fun calculate_tip_rounded() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.option_fifteen_percent))
+            .perform(click())
+
+        onView(withId(R.id.option_fifteen_percent))
+            .check(matches(isChecked()))
+
+        onView(withId(R.id.option_twenty_percent))
+            .check(matches(isNotChecked()))
+
+        onView(withId(R.id.option_eighteen_percent))
+            .check(matches(isNotChecked()))
+
+        onView(withId(R.id.round_up_switch))
+            .perform(click())
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId((R.id.tip_result)))
+            .check(matches(withText(containsString("$8.00"))))
     }
 }
